3433d5a942decd4b86557f9850e4333b343976ae
chandrika <modugumudi-leela.chandrika@capgemini.com>
2023-03-03 14:25:10 +0530

    Firts Commit

---
+++ Controllers/employeesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CargoManagement;
+
+using SolrNet.Utils;
+
+namespace CargoManagement.Controllers
+{
+    [Authorize]
+    public class employeesController : Controller
+    {
+        
+        private Model1 db = new Model1();
+
+        public ActionResult Display()
+        {
+            return View();
+        }
+        public ActionResult Display1()
+        {
+            return View();
+        }
+
+
+
+
+
+        [Authorize(Roles = "Admin,Employee")]
+        public ActionResult Index( string searchString )
+        {
+            var employees = from n in db.employees
+                            select n;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                employees = employees.Where(s => s.emp_name.Contains(searchString));
+            }
+            return View(employees);
+        }
+
+        // GET: employees/Details/5
+        [Authorize(Roles = "Admin,Employee,Customer")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            employee employee = db.employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: employees/Create
+        [Authorize(Roles = "Admin,Employee")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin,Employee")]
+        public ActionResult Create([Bind(Include = "emp_id,emp_name,emp_department,emp_salary,emp_mobile,activestatus")] employee Employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.employees.Add(Employee);
+                db.SaveChanges();
+                return RedirectToAction("Display1");
+            }
+
+            return View(Employee);
+        }
+
+        // GET: employees/Edit/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            employee employee = db.employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit([Bind(Include = "emp_id,emp_name,emp_department,emp_salary,emp_mobile,activestatus")] employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: employees/Delete/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            employee employee = db.employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: employees/Delete/5
+        [Authorize(Roles = "Admin")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            employee employee = db.employees.Find(id);
+            db.employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
